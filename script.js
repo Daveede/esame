0896c348057734f3fe9cf78bffa6034a0d8b13b3
Dave <xspreadz@gmail.com>
2020-02-07 12:24:46 +0100

    about to end myself :)

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Instantiate MDC Drawer
-const drawerEl = document.querySelector('.mdc-drawer');
-const drawer = new mdc.drawer.MDCDrawer.attachTo(drawerEl);
-
-// Instantiate MDC Top App Bar (required)
-const topAppBarEl = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarEl);
-
-topAppBar.setScrollTarget(document.querySelector('.main-content'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-  drawer.open = !drawer.open;
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture").change(function(){
+        readURL(this);
+    });
 });
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
